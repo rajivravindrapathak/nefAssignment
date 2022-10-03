c91b65f31b03ce237a18a086b46e953748aadf20
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-03 11:50:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Neftx from './Components/Neftx'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Neftx />
+    </div>
+  )
+}
+
+export default App
