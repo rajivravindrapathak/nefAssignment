867651f8cc7cd5b1c9d803893462cbf205f294cb
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-08 15:34:48 +0530

    Add files via upload

--- src/Components/Discount.jsx
+++ src/Components/Discount.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import { Link } from 'react-router-dom'
 import "./Discount.css"
 
 const Discount = ({ image, Receipe, price, discount }) => {
+  const [amt, setAmt] = useState()
+  let amount
   const btnDiscount = () => {
-
+      amount = price - Math.floor((price*discount)/100)
+      console.log("amount", amount)
+      setAmt(amount)
   }
-    
+  
+  // console.log("amount", amount)
+  
   return (
     // <Link to={"/ProductPage"}> 
     
@@ -16,7 +22,8 @@ const Discount = ({ image, Receipe, price, discount }) => {
         <h2>price: {price}</h2>
         <h2>discount: {discount}%</h2>
         <hr />
-        <button onClick={btnDiscount}>Discount</button>
+        <button onClick={btnDiscount}>Paybale amount</button>
+        <h2>amount: {amt}</h2>
       </div>
     // </Link>
   )
