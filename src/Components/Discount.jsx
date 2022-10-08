70d145b386acf55ad0a4d3af859f70b2015d5802
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-08 10:28:58 +0530

    Add files via upload

--- src/Components/Discount.jsx
+++ src/Components/Discount.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import "./Discount.css"
 
 const Discount = ({ image, Receipe, price, discount }) => {
+  const btnDiscount = () => {
+
+  }
     
   return (
     // <Link to={"/ProductPage"}> 
     
-      <div style={{marginTop:"10px"}} >
-      
+      <div className='dis'>
         <img src={image} alt="img book"/>
         <h2>Receipe: {Receipe}</h2>
         <h2>price: {price}</h2>
         <h2>discount: {discount}</h2>
         <hr />
+        <button onClick={btnDiscount}>Discount</button>
       </div>
     // </Link>
   )
